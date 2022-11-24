e1a2da038f21d7dff14207be8d8d2eb0084aaf16
Alexis Delgado <alexisdelgado@MacBook-Pro-7.local>
2022-11-23 22:41:55 -0800

    added form page, added back links to homepage

--- views/index.jsx
+++ views/index.jsx
@@ -21,8 +21,13 @@ function Index ({breads, title}) {
             }
     
             </ul>
+            <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
             </Default>
     )
 }
+
+
 
 module.exports = Index
